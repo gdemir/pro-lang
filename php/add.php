982f9160ae960ff32aab028324b277528dfdc865
Gökhan Demir <gdemir@bil.omu.edu.tr>
2011-05-03 23:36:28 +0300

    minor

--- php/add.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo !($connector = mysql_connect('localhost','root','12345')) ? 'Can\'t find ' . mysql_error() : 'databate succesfully connect';
-echo !(mysql_select_db('ogrenci', $connector)) ? 'Can\'t find ' . mysql_error() : 'database select succesfully ';
-$ad = $_POST['ad'];
-$ps = $_POST['ps'];
-mysql_query("insert into kul (ad, sifre) VALUES('$ad' , '$ps')");
-?>
